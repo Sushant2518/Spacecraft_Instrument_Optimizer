40c77e5e4232698ba1da6d75e2442b14bff4bb2e
Sushant2518 <sushantnarvekar3118@gmail.com>
2024-05-01 16:05:47 +0530

    done

--- Spacecraft_Instrument_Optimizer/src/com/masai/SelectInstrument.java
+++ Spacecraft_Instrument_Optimizer/src/com/masai/SelectInstrument.java
@@ -1,76 +1,59 @@
 package com.masai;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class SelectInstrument {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		// available instuments
-		Instruments[] instruments = {
-                new Instruments(3, 2, 10),
-                new Instruments(4, 3, 15),
-                new Instruments(2, 1, 8),
-                new Instruments(5, 4, 20)
-        };
-		
-		
-		//Capacity
-		int maxWeight = 10;
-		int maxVolume = 7;
-		
-		//optimal combination of instruments
-		List<Instruments> selectedInstruments = selectInstruments(instruments, maxWeight, maxVolume);
-		
-		
-		System.out.println("Selected Instruments: ");
-		
-		for (Instruments instrument : selectedInstruments) {
-            System.out.println("Weight: " + instrument.weight + " kg, Volume: " + instrument.volume + " m^3, Scientific Value: " + instrument.baseScintificValue);
-        }
-		
-		int totalWeight = selectedInstruments.stream().mapToInt(instrument -> instrument.weight).sum();
-        int totalVolume = selectedInstruments.stream().mapToInt(instrument -> instrument.volume).sum();
-        int totalBaseScintificValue = selectedInstruments.stream().mapToInt(instrument -> instrument.baseScintificValue).sum();
+		List<Instruments> instruments = new ArrayList<>();
+        instruments.add(new Instruments(3, 2, 10));
+        instruments.add(new Instruments(4, 3, 15));
+        instruments.add(new Instruments(2, 1, 8));
+        instruments.add(new Instruments(5, 4, 20));
 
-        System.out.println("Total Weight: " + totalWeight + " kg");
-        System.out.println("Total Volume: " + totalVolume + " m^3");
-        System.out.println("Total Scientific Value: " + totalBaseScintificValue);
+        int payloadCapacity = 10;
+        int volumeCapacity = 7;
+
+        List<Instruments> selectedInstruments = selectInstruments(instruments, payloadCapacity, volumeCapacity);
+        System.out.println("Selected Instruments:");
+        for (Instruments instrument : selectedInstruments) {
+            System.out.println("Weight: " + instrument.weight + ", Volume: " + instrument.volume +
+                    ", Base Scientific Value: " + instrument.baseScintificValue);
+        }
+
+        int totalWeight = selectedInstruments.stream().mapToInt(instrument -> instrument.weight).sum();
+        int totalVolume = selectedInstruments.stream().mapToInt(instrument -> instrument.volume).sum();
+        int totalScientificValue = selectedInstruments.stream().mapToInt(instrument -> instrument.baseScintificValue).sum();
+        
+        System.out.println("Total Weight: " + totalWeight);
+        System.out.println("Total Volume: " + totalVolume);
+        System.out.println("Total Scientific Value: " + totalScientificValue);
+	
 	}
 	
-	public static List<Instruments> selectInstruments(Instruments[] instruments, int maxWeight, int maxVolume){
-		List<Instruments> bestCombination = new ArrayList<>();
-		
-		int bestValue = 0;
-		
-		for (int i = 0; i < (1 << instruments.length); i++) {
-			List<Instruments> currentCombination = new ArrayList<>();
-			
-			int currentWeight = 0;
-            int currentVolume = 0;
-            int currentBaseScintificValue = 0;
-            
-            for (int j = 0; j < instruments.length; j++) {
-                if ((i & (1 << j)) != 0) {
-                    Instruments instrument = instruments[j];
-                    if (currentWeight + instrument.weight <= maxWeight && currentVolume + instrument.volume <= maxVolume) {
-                        currentCombination.add(instrument);
-                        currentWeight += instrument.weight;
-                        currentVolume += instrument.volume;
-                        currentBaseScintificValue += instrument.baseScintificValue;
-                    }
-                }
-            }
-            if (currentBaseScintificValue > bestValue) {
-                bestValue = currentBaseScintificValue;
-                bestCombination = new ArrayList<>(currentCombination);
-            }
-            
-		}
-		return bestCombination;
-	}
+	 private static List<Instruments> selectInstruments(List<Instruments> instruments, int payloadCapacity, int volumeCapacity) {
+	        Collections.sort(instruments, Comparator.comparingDouble(instrument ->
+	                (double) instrument.baseScintificValue / instrument.weight));
+
+	        List<Instruments> selectedInstruments = new ArrayList<>();
+	        int currentPayload = 0;
+	        int currentVolume = 0;
+
+	        for (Instruments instrument : instruments) {
+	            if (currentPayload + instrument.weight <= payloadCapacity &&
+	                    currentVolume + instrument.volume <= volumeCapacity) {
+	                selectedInstruments.add(instrument);
+	                currentPayload += instrument.weight;
+	                currentVolume += instrument.volume;
+	            }
+	        }
+
+	        return selectedInstruments;
+	    }
 
 }
 
